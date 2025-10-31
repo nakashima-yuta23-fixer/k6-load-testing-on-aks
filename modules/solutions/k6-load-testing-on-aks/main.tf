ad2dd57f0ca979ea6402a9252e3d686d33677d82
nishihan-ryoga25-fixer <nishihan.ryoga25@fixer.co.jp>
2025-10-31 14:40:14 +0900

    NAT Gateway、IP Address Prefixをsolutionsファイル上にハードコーディングするように変更

--- modules/solutions/k6-load-testing-on-aks/main.tf
+++ modules/solutions/k6-load-testing-on-aks/main.tf
@@ -18,7 +18,7 @@ module "resource_group" {
 }
 
 # vnet/subnet 作成
-module "vent" {
+module "vnet" {
   source = "../../foundations/networking_vnet"
 
   resource_group_name = module.resource_group.name
@@ -43,114 +43,135 @@ module "vent" {
 
 # peering 作成 (prometheus でメトリックを Grafana にエクスポートするため)
 
-# acr 作成
-# TODO: foudationsモジュールから呼び出せるようにする。
-resource "azurerm_container_registry" "this" {
-  name                = "crk6testloadtestingdvje"
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
-  sku                 = "Standard"
-  admin_enabled       = false
+# # acr 作成
+# # TODO: foudationsモジュールから呼び出せるようにする。
+# resource "azurerm_container_registry" "this" {
+#   name                = "crk6testloadtestingdvje"
+#   resource_group_name = module.resource_group.name
+#   location            = module.resource_group.location
+#   sku                 = "Standard"
+#   admin_enabled       = false
+
+#   lifecycle {
+#     ignore_changes = [tags]
+#   }
+# }
+
+# # aks クラスター作成
+# # TODO: foudationsモジュールから呼び出せるようにする。
+# resource "azurerm_kubernetes_cluster" "this" {
+#   name                = "aks-load-testing-dv-je"
+#   location            = module.resource_group.location
+#   resource_group_name = module.resource_group.name
+#   dns_prefix          = "aks-load-testing-dv-je-dns"
+
+#   default_node_pool {
+#     name                 = "npsystem"
+#     node_count           = 2
+#     vm_size              = "Standard_D8ls_v5"
+#     auto_scaling_enabled = false
+#     max_pods             = 110
+#     os_disk_type         = "Managed"
+#     os_sku               = "Ubuntu"
+#     type                 = "VirtualMachineScaleSets"
+#     upgrade_settings {
+#       drain_timeout_in_minutes      = 0
+#       max_surge                     = "10%"
+#       node_soak_duration_in_minutes = 0
+#     }
+#     vnet_subnet_id = module.vnet.subnet_ids["cluster-k8s"]
+#     zones          = [1]
+#   }
+
+#   api_server_access_profile {
+#     authorized_ip_ranges = ["202.211.86.16/32"]
+#   }
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+#   image_cleaner_enabled = false # 負荷試験実行基盤用のAKSは負荷試験が終わり次第クラスターを削除する予定のため
+
+#   # 常に最新バージョンを使用するため、kubernetes_versionは指定せず常に最新バージョンでAKSを構築する。
+#   # kubernetes_version = "x.x.x"
+
+#   local_account_disabled = false
+
+#   network_profile {
+#     network_plugin      = "azure"
+#     network_data_plane  = "azure"
+#     network_plugin_mode = "overlay"
+#   }
+
+#   bootstrap_profile {
+#     artifact_source = "Direct"
+#   }
+
+#   node_os_upgrade_channel = "None"
+
+#   oidc_issuer_enabled               = false
+#   open_service_mesh_enabled         = false
+#   workload_identity_enabled         = false
+#   role_based_access_control_enabled = true
+#   sku_tier                          = "Standard"
+
+#   storage_profile {
+#     blob_driver_enabled         = true
+#     disk_driver_enabled         = true
+#     file_driver_enabled         = true
+#     snapshot_controller_enabled = true
+#   }
+
+#   support_plan = "KubernetesOfficial"
+
+#   lifecycle {
+#     ignore_changes = [tags]
+#   }
+# }
+
+# output "object_id_of_managed_id_for_aks" {
+#   description = "Object ID of system-assigned managed identity for AKS cluster"
+#   value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+# }
+
+# nat gateway 作成
+resource "azurerm_nat_gateway" "this" {
+  name                    = "ng-k6natg-load-testing-dv-je"
+  resource_group_name     = module.resource_group.name
+  location                = module.resource_group.location
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 4
 
   lifecycle {
     ignore_changes = [tags]
   }
-}
-
-# aks クラスター作成
-# TODO: foudationsモジュールから呼び出せるようにする。
-resource "azurerm_kubernetes_cluster" "this" {
-  name                = "aks-load-testing-dv-je"
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
-  dns_prefix          = "aks-load-testing-dv-je-dns"
-
-  default_node_pool {
-    name                 = "npsystem"
-    node_count           = 2
-    vm_size              = "Standard_D8ls_v5"
-    auto_scaling_enabled = false
-    max_pods             = 110
-    os_disk_type         = "Managed"
-    os_sku               = "Ubuntu"
-    type                 = "VirtualMachineScaleSets"
-    upgrade_settings {
-      drain_timeout_in_minutes      = 0
-      max_surge                     = "10%"
-      node_soak_duration_in_minutes = 0
-    }
-    vnet_subnet_id = module.vent.subnet_ids["cluster-k8s"]
-    zones          = [1]
-  }
-
-  api_server_access_profile {
-    authorized_ip_ranges = ["202.211.86.16/32"]
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  image_cleaner_enabled = false # 負荷試験実行基盤用のAKSは負荷試験が終わり次第クラスターを削除する予定のため
-
-  # 常に最新バージョンを使用するため、kubernetes_versionは指定せず常に最新バージョンでAKSを構築する。
-  # kubernetes_version = "x.x.x"
-
-  local_account_disabled = false
-
-  network_profile {
-    network_plugin      = "azure"
-    network_data_plane  = "azure"
-    network_plugin_mode = "overlay"
-  }
-
-  bootstrap_profile {
-    artifact_source = "Direct"
-  }
-
-  node_os_upgrade_channel = "None"
-
-  oidc_issuer_enabled               = false
-  open_service_mesh_enabled         = false
-  workload_identity_enabled         = false
-  role_based_access_control_enabled = true
-  sku_tier                          = "Standard"
-
-  storage_profile {
-    blob_driver_enabled         = true
-    disk_driver_enabled         = true
-    file_driver_enabled         = true
-    snapshot_controller_enabled = true
-  }
-
-  support_plan = "KubernetesOfficial"
-
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
-
-output "object_id_of_managed_id_for_aks" {
-  description = "Object ID of system-assigned managed identity for AKS cluster"
-  value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 }
 
 # public IP Address Prefix 作成
+resource "azurerm_public_ip_prefix" "public_ip_prefix" {
+  name                = "pip-k6natg-load-testing-dv-je"
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
 
-# nat gateway 作成
-module "nat_gateway" {
-  source = "../../foundations/networking_nat_gateway"
+  prefix_length       = 30
+  sku                 = "Standard"
 
-  resource_group_name     = module.resource_group.name
-  location                = module.resource_group.location
-  customer_code           = var.customer_code
-  role                    = var.role
-  environment             = var.environment
-  sku_name                = var.nat_gateway_sku_name
-  idle_timeout_in_minutes = var.nat_gateway_idle_timeout_in_minutes
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
 
-  is_ip_address_prefix = false
-  subnet_id            = {for k, v in module.vent.subnet_ids : k => v if k != "gateway-k8s"}
+# public IP Address Prefixとnat gateway紐づけ
+resource "azurerm_nat_gateway_public_ip_prefix_association" "public_ip_prefix_association" {
+  nat_gateway_id      = azurerm_nat_gateway.this.id
+  public_ip_prefix_id = azurerm_public_ip_prefix.public_ip_prefix.id
+}
+
+# subnetとnat gateway紐づけ
+resource "azurerm_subnet_nat_gateway_association" "subnet_association" {
+  subnet_id      = module.vnet.subnet_ids["cluster-k8s"]
+  nat_gateway_id = azurerm_nat_gateway.this.id
 }
 
 # k6-operatorのインストール (helmでインストールしたものもtfstateで管理するのか微妙？頻繁に実行基盤が削除されるのであれば、ｄelete時にhelmでインストールしたものの状態が変更していれば正常に削除できない可能性がある。)
