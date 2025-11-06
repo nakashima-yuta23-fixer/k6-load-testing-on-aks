ce5bd36bd25439e734150c32261fc6de96e04888
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2025-11-06 16:57:04 +0900

    k8s and k6

--- modules/solutions/k6-load-testing-on-aks/outputs.tf
+++ modules/solutions/k6-load-testing-on-aks/outputs.tf
@@ -4,3 +4,9 @@
 # Provides key attributes of the created Container App Environment and its
 # associated resources, making them easily consumable by other modules.
 # ==============================================================================
+
+# AKS
+output "object_id_of_managed_id_for_aks" {
+  description = "Object ID of system-assigned managed identity for AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
